d494d17267f3325debe3e78ec37fc5e6e887a335
Slivca_1 <137814344+OlegTXT@users.noreply.github.com>
2023-07-05 21:07:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ class Human:
         self.city = city,
         self.country = country,
         self.home_address = home_address
+    def SHUSHIN(self):
+        try:
+            return self.name, self.surname, self.date, self.contact_telephone, self.city, self.country, self.home_address
+        except:
+            print("No name, no surname, no date, no contact_telephone, no city, no country, no home_address")
 
 
-
+a = Human("Georgiy", "Shushin", "29.02.2000", "907658234", "Tachkent", "Uzbekistan", "Prospect Shushina")
+print(a.name, a.surname, a.date, a.contact_telephone, a.city, a.country, a.home_address)
