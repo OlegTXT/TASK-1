1b42c02d5b271411822ba22185ba1b98e2427a32
Олег Эм <@corp.itstep>
2023-07-05 19:54:32 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+class Human:
+    def __init__(self, name, surname, date, contact_telephone, city, country, home_address):
+        self.name = name,
+        self.surname = surname,
+        self.date =date,
+        self.contact_telephone = contact_telephone,
+        self.city = city,
+        self.country = country,
+        self.home_address = home_address
+
+
+
